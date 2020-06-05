7a72cfd6c543a0785393713ce0a109f3fc70b2f3
Matt Gaddes <mgaddes@absolute.com>
2020-06-05 16:22:15 -0700

    Refactor and remove unused code

--- handler.js
+++ handler.js
@@ -7,17 +7,6 @@ const defaultHeaders = {
   'Access-Control-Allow-Credentials': true,
 };
 
-module.exports.endpoint = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
-    }),
-  };
-
-  callback(null, response);
-};
-
 module.exports.getUser = (event, context, callback) => {
   const params = {
     Key: {
--- testHelpers.js
+++
@@ -1,6 +0,0 @@
-// This file contains command line snippets for testing serverless functions locally
-
-// Send user ID in request body
-//  Function: POST createUser
-//  Params: { "id": "matt" }
-export const createUser = `serverless invoke local -f createUser -d '{"body": "{\"id\": \"matt\"}"}' -l`;
